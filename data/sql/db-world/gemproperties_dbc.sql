68acd98a766e24688655d7eadfc60c99302aa26b
CreamCorp <creamcorp@atomicmail.io>
2026-06-27 01:44:30 -0700

    add initial sql scripts

---
+++ data/sql/db-world/gemproperties_dbc.sql
@@ -0,0 +1,17 @@
+DELETE FROM `gemproperties_dbc` WHERE `ID` BETWEEN 1630 AND 1644;
+
+INSERT INTO `gemproperties_dbc` VALUES (1630,3885,0,0,14);
+INSERT INTO `gemproperties_dbc` VALUES (1631,3886,0,0,14);
+INSERT INTO `gemproperties_dbc` VALUES (1632,3887,0,0,14);
+INSERT INTO `gemproperties_dbc` VALUES (1633,3888,0,0,14);
+INSERT INTO `gemproperties_dbc` VALUES (1634,3889,0,0,14);
+INSERT INTO `gemproperties_dbc` VALUES (1635,3890,0,0,14);
+INSERT INTO `gemproperties_dbc` VALUES (1636,3891,0,0,2);
+INSERT INTO `gemproperties_dbc` VALUES (1637,3892,0,0,4);
+INSERT INTO `gemproperties_dbc` VALUES (1638,3893,0,0,8);
+INSERT INTO `gemproperties_dbc` VALUES (1639,3894,0,0,6);
+INSERT INTO `gemproperties_dbc` VALUES (1640,3895,0,0,6);
+INSERT INTO `gemproperties_dbc` VALUES (1641,3896,0,0,6);
+INSERT INTO `gemproperties_dbc` VALUES (1642,3897,0,0,12);
+INSERT INTO `gemproperties_dbc` VALUES (1643,3898,0,0,12);
+INSERT INTO `gemproperties_dbc` VALUES (1644,3899,0,0,12);
---
+++ data/sql/db-world/spellitemenchantment_dbc.sql
@@ -0,0 +1,10 @@
+DELETE FROM `spellitemenchantment_dbc` WHERE `ID` BETWEEN 3894 AND 3901;
+
+INSERT INTO `spellitemenchantment_dbc` VALUES (3894,0,3,0,0,0,0,0,0,0,0,90002,0,0,'+1% Chance to find special items.','','','','','','','','','','','','','','','',16712190,0,0,60037,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3895,0,3,0,0,0,0,0,0,0,0,90001,0,0,'+5% Chance to find special items.','','','','','','','','','','','','','','','',16712190,0,0,60038,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3896,0,3,0,0,0,0,0,0,0,0,90000,0,0,'+10% Chance to find special items.','','','','','','','','','','','','','','','',16712190,0,0,60039,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3897,0,3,0,0,0,0,0,0,0,0,90003,0,0,'+10% Experience from kills and quests.','','','','','','','','','','','','','','','',16712190,0,0,60042,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3898,0,3,0,0,0,0,0,0,0,0,90004,0,0,'+5% Experience from kills and quests.','','','','','','','','','','','','','','','',16712190,0,0,60041,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3899,0,3,0,0,0,0,0,0,0,0,90005,0,0,'+1% Experience from kills and quests.','','','','','','','','','','','','','','','',16712190,0,0,60040,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3900,0,3,0,0,100,0,0,100,0,0,90007,0,0,'Lifeburst 1','','','','','','','','','','','','','','','',16712190,0,9,0,0,0,0,0);
+INSERT INTO `spellitemenchantment_dbc` VALUES (3901,0,3,0,0,100,0,0,100,0,0,90010,0,0,'Thunderburst 1','','','','','','','','','','','','','','','',16712190,0,9,0,0,0,0,0);
